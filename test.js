27d7141a6084d5bfac2428fca9902ccaebcfdfb2
oskab <113323047+oskab@users.noreply.github.com>
2022-09-12 09:55:45 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-console.log('hello')
+$(document).ready(function () {
+    console.log('hello');
+     $('#formLogin').submit(function(){
+            var pass = $('#txtPassword').val();
+            var username = $('.form-control').val();
+       
+            $.ajax({ 
+                type: "POST",
+                data : username+';'+pass
+                url: "/eproc4/hello", 
+                success: function(data){
+                  console.log(data)
+                }
+            });
+       
+     })
+});
